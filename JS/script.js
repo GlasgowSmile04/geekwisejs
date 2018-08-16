f98ad9a16db378975402a3d76d8cfd7a85eb382f
Zachariah Glasgow <zac@Zachariahs-MacBook-Pro.local>
2018-08-15 20:52:10 -0700

    TIL some MATH() and popups

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
 console.log("Hello World!")
+
+//TIL How to do variables, prompts, and alerts.
+var name = prompt('What is your name?');
+alert(name);
+
+//Also I learned some MATH functions that are baked into JS
+var rand = Math.floor(Math.random() * 10)
+console.log(rand)
