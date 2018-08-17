ee898ce80899b9628114a5c081f5251dae0e35da
Zachariah Glasgow <zac@Zachariahs-MacBook-Pro.local>
2018-08-16 21:19:08 -0700

    Basic assignment complete - no bonus points. Need to add Date()functionality

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,15 @@
-console.log("Hello World!")
+// Asks for the user's first name
+var firstName = prompt("What is your first name?");
+// // Asks for the user's last name
+var lastName = prompt("What is your last name?");
+// // Logs the user's first name to the console
+console.log(firstName);
+// // Alerts the user's last name
+alert(lastName);
+// *Asks for the user's birthday, confirms whether the birthday recieved is correct, and alerts the resulting birthday
+var birthYear = prompt("What year were you born in?");
+var birthMonth = prompt("What month were you born in?");
+var birthDay = prompt("What day were you born on?");
 
-//TIL How to do variables, prompts, and alerts.
-var name = prompt('What is your name?');
-alert(name);
-
-//Also I learned some MATH functions that are baked into JS
-var rand = Math.floor(Math.random() * 10)
-console.log(rand)
+var dateOfBirth = (birthMonth + " " + birthDay + " " + birthYear);
+alert("Your birthday is: " + dateOfBirth);
