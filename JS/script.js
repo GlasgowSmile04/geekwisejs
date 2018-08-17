47a299e742451362ea23ba79284383c3dd97c6ec
Zachariah Glasgow <zac@Zachariahs-MacBook-Pro.local>
2018-08-16 21:36:57 -0700

    Bonus points acheived, date func included

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,21 @@
-// Asks for the user's first name
+// // Asks for the user's first name
 var firstName = prompt("What is your first name?");
-// // Asks for the user's last name
-var lastName = prompt("What is your last name?");
-// // Logs the user's first name to the console
+// // // Logs the user's first name to the console
 console.log(firstName);
-// // Alerts the user's last name
+// // // Asks for the user's last name
+var lastName = prompt("What is your last name?");
+// // // Alerts the user's last name
 alert(lastName);
 // *Asks for the user's birthday, confirms whether the birthday recieved is correct, and alerts the resulting birthday
-var birthYear = prompt("What year were you born in?");
-var birthMonth = prompt("What month were you born in?");
-var birthDay = prompt("What day were you born on?");
+// var birthYear = prompt("What year were you born in?");
+// var birthMonth = prompt("What month were you born in?");
+// var birthDay = prompt("What day were you born on?");
+//
+// var dateOfBirth = (birthMonth + " " + birthDay + " " + birthYear);
+// alert("Your birthday is: " + dateOfBirth);
 
-var dateOfBirth = (birthMonth + " " + birthDay + " " + birthYear);
-alert("Your birthday is: " + dateOfBirth);
+//Self-parsing function built into JS? Awesome.
+var test = prompt(`What is your birtday?
+mm/DD/yyyy`)
+var bd = new Date(test);
+alert(bd);
