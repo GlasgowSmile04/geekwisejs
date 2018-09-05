2181cb091ba6373260b4585a2308a8ca0c461def
Zachariah Glasgow <zac@Zachariahs-MacBook-Pro.local>
2018-09-04 21:47:25 -0700

    That was pretty easy

--- JS/script.js
+++ JS/script.js
@@ -433,95 +433,95 @@
 
 //Day 6 Takehome
 // Ask your user to give you a scale (Celcius or Fahrenheit) and the tempurature they want to convert. So if they give you 90 Fahrenheit, they would want you to convert 90 degrees Fahrenheit to whatever that is in Celcius
-var temp;
-var scale;
-var result;
-let tempDisplay = document.querySelector(".temp");
-
-getTemp();
-getScale();
-setResult();
-
-var newResult = convert(temp, scale);
-
-function getTemp(){
-  let t = parseInt(prompt(`What is the temperature you would like to convert?`));
-    if(isNaN(t)){
-      alert(`That input is not a number. Please enter a degree in NUMBERS!`)
-      getTemp();
-    } else {
-      temp = t;
-      return t;
-    };
-};
-
-function getScale(){
- let s = prompt(`Which scale would you like to convert FROM? \nPlease input C or F. \n (Result will be the opposite scale)`);
- s = s.toUpperCase();
-   if(s === "C" || s === "F"){
-     scale = s;
-     return s;
-   }else {
-     alert("Invalid entry")
-     getScale();
-   }
-};
-// Employ a function that accepts those two values as arguments: temperature and scale (either celcius or fahrenheit) and converts the tempurature they gave you to the opposite scale.
-
-function convert(temp, scale){
-  let newTemp;
-  let newScale;
-  let newResult;
-  if(scale === "C"){
-    newTemp = parseInt(Math.floor((temp * 1.8) + 32));
-    newScale = "F";
-    newResult = `${newTemp}${newScale}`
-  } else {
-    newTemp = parseInt(Math.floor((temp - 32) / 1.8));
-    newScale = "C";
-    newResult = `${newTemp}${newScale}`
-  };
-  return newResult;
-};
-
-
-// Display the temperature in an h3 with minimal styling.
-function setResult(){
-  result = `${temp}${scale}`;
-}
-
-tempDisplay.textContent = result
-
-// From that point on, when the user clicks on the h3, run the function to convert the temperature back and forth between Celcius and Fahrenheit.
-function changeDisplay(){
-  if(tempDisplay.textContent == result){
-    tempDisplay.textContent = newResult;
-  } else {
-    tempDisplay.textContent = result;
-  };
-};
-
-document.querySelector("#changer").addEventListener("click", changeDisplay);
-
-// Display the converted temperature in the h3 each time it is changed.
-
-function reset(){
-  temp = "";
-  scale = "";
-  result = "";
-  newResult = "";
-  let tempDisplay = document.querySelector(".temp");
-
-  getTemp();
-  getScale();
-  setResult();
-
-  newResult = convert(temp, scale);
-
-  tempDisplay.textContent = result
-};
-
-document.querySelector("#reset").addEventListener("click", reset);
+// var temp;
+// var scale;
+// var result;
+// let tempDisplay = document.querySelector(".temp");
+//
+// getTemp();
+// getScale();
+// setResult();
+//
+// var newResult = convert(temp, scale);
+//
+// function getTemp(){
+//   let t = parseInt(prompt(`What is the temperature you would like to convert?`));
+//     if(isNaN(t)){
+//       alert(`That input is not a number. Please enter a degree in NUMBERS!`)
+//       getTemp();
+//     } else {
+//       temp = t;
+//       return t;
+//     };
+// };
+//
+// function getScale(){
+//  let s = prompt(`Which scale would you like to convert FROM? \nPlease input C or F. \n (Result will be the opposite scale)`);
+//  s = s.toUpperCase();
+//    if(s === "C" || s === "F"){
+//      scale = s;
+//      return s;
+//    }else {
+//      alert("Invalid entry")
+//      getScale();
+//    }
+// };
+// // Employ a function that accepts those two values as arguments: temperature and scale (either celcius or fahrenheit) and converts the tempurature they gave you to the opposite scale.
+//
+// function convert(temp, scale){
+//   let newTemp;
+//   let newScale;
+//   let newResult;
+//   if(scale === "C"){
+//     newTemp = parseInt(Math.floor((temp * 1.8) + 32));
+//     newScale = "F";
+//     newResult = `${newTemp}${newScale}`
+//   } else {
+//     newTemp = parseInt(Math.floor((temp - 32) / 1.8));
+//     newScale = "C";
+//     newResult = `${newTemp}${newScale}`
+//   };
+//   return newResult;
+// };
+//
+//
+// // Display the temperature in an h3 with minimal styling.
+// function setResult(){
+//   result = `${temp}${scale}`;
+// }
+//
+// tempDisplay.textContent = result
+//
+// // From that point on, when the user clicks on the h3, run the function to convert the temperature back and forth between Celcius and Fahrenheit.
+// function changeDisplay(){
+//   if(tempDisplay.textContent == result){
+//     tempDisplay.textContent = newResult;
+//   } else {
+//     tempDisplay.textContent = result;
+//   };
+// };
+//
+// document.querySelector("#changer").addEventListener("click", changeDisplay);
+//
+// // Display the converted temperature in the h3 each time it is changed.
+//
+// function reset(){
+//   temp = "";
+//   scale = "";
+//   result = "";
+//   newResult = "";
+//   let tempDisplay = document.querySelector(".temp");
+//
+//   getTemp();
+//   getScale();
+//   setResult();
+//
+//   newResult = convert(temp, scale);
+//
+//   tempDisplay.textContent = result
+// };
+//
+// document.querySelector("#reset").addEventListener("click", reset);
 
 // function init(){
 //   var temp;
@@ -538,3 +538,221 @@ document.querySelector("#reset").addEventListener("click", reset);
 //
 //   tempDisplay.textContent = result
 // };
+
+//Day 7
+//GRAB INNER TEXT
+// let text = document.getElementById("para").textContent;
+// document.getElementById("para").textContent = "This is some other text"
+
+//GRAB INNER HTML
+// let text = document.getElementById("para").innerHTML;
+// console.log(text);
+//
+// let content = document.getElementById("content");
+// //CREATE A PARAGRAPH TAG
+// let myPTag = document.createElement("p");
+// //DEFINE THE CONTENT WITHIN THE NEW ELEMENT
+// myPTag.textContent = "This is a test."
+// //PUSH THE NEW ELEMENT INTO THE DOM
+// content.appendChild(myPTag);
+//
+//
+// //CREATING A BUTTON AND ADDING A CLASS.
+// let myBtn = document.createElement("button");
+// myBtn.className = "btn btn-primary";
+// myBtn.id = "someId";
+// myBtn.textContent = "click me";
+//
+// myBtn.classList.remove("btn-primary");
+// myBtn.classList.add("btn-primary");
+//
+// content.appendChild(myBtn);
+//
+// //USING SET ATTRIBUTE
+// let myImg = document.createElement("img");
+// myImg.setAttribute("alt", "Add attribute from JS");
+// myImg.setAttribute("src", "https://pbs.twimg.com/profile_images/584025370036314112/0maBx-gf_400x400.jpg");
+// content.appendChild(myImg);
+//
+// console.log(myImg.getAttribute("src"))
+// const script = document.querySelector("script");
+// const body = document.querySelector("body");
+//
+// let newP = document.createElement("p");
+// let newH1 = document.createElement("h1");
+// let newFooter = document.createElement("footer");
+// let newHeader = document.createElement("header");
+// let newUl = document.createElement("ul");
+// let newDiv = document.createElement("div");
+//
+// function createLi(classArray, linkText){
+//   let newLink = document.createElement("li");
+//   let newA = document.createElement("a");
+//   newA.setAttribute("href", "#");
+//   newA.textContent = linkText;
+//   newLink.appendChild(newA);
+//   for (let i = 0; i < classArray.length; i++) {
+//     newLink.classList.add(classArray[i]);
+//   }
+//   newUl.appendChild(newLink);
+// }
+//
+// createLi(["pull-left"], "Home");
+// createLi(["pull-right"], "Contact");
+// createLi(["pull-right"], "About");
+//
+// newUl.classList.add("nav", "nav-pills");
+// newHeader.appendChild(newUl);
+// body.insertBefore(newHeader, script);
+//
+//
+// function createTextElement(elementType, text) {
+//   let newElement = document.createElement(elementType);
+//   newElement.textContent = text;
+//   return newElement;
+// }
+//
+// newDiv.classList.add("text-center");
+//
+// let h1 = createTextElement("h1", "Hello World!");
+// let p = createTextElement("p", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+// newDiv.appendChild(h1);
+// newDiv.appendChild(p);
+// body.insertBefore(newDiv, script);
+//
+// let newFooterLink = document.createElement("a");
+//
+// newFooterLink.textContent = "Check me out.";
+// newFooterLink.setAttribute("href", "https://www.google.com");
+// newFooterLink.setAttribute("target", "_blank");
+// newFooterLink.classList.add("text-center", "footer");
+// newFooter.appendChild(newFooterLink);
+// body.insertBefore(newFooter, script);
+
+
+//THIS
+// function myFunc() {
+//   console.log("myFunc", this);
+// }
+// myFunc()
+//
+// console.log(this)
+//
+// function fizzBuzz(){
+//   let list = document.createElement("ol");
+//   for (let i = 1; i < 101; i++) {
+//     if (i % 15 == 0) {
+//       let res = document.createElement("li");
+//       res.textContent = "FizzBuzz";
+//       list.appendChild(res)
+//       console.log("FizzBuzz");
+//     } else if (i % 5 == 0) {
+//       let res = document.createElement("li");
+//       res.textContent = "Buzz";
+//       list.appendChild(res)
+//       console.log("Buzz");
+//     }else if (i % 3 == 0) {
+//       let res = document.createElement("li");
+//       res.textContent = "Fizz";
+//       list.appendChild(res)
+//       console.log("Fizz");
+//     } else {
+//       console.log(i)
+//       let res = document.createElement("li")
+//       res.textContent = i;
+//       list.appendChild(res)
+//     }
+//   }
+//   body.insertBefore(list, script);
+// }
+
+//Day 7 Takehome
+const script = document.querySelector("script")
+const body = document.querySelector("body")
+let myBtn = document.querySelector(".btn")
+let newH1 = document.createElement("h1")
+let newDiv = document.createElement("div")
+let newP = document.createElement("p")
+
+function generateH1(){
+  // When the user clicks a button, prompt them for the a title.
+    let titleText = prompt("Please title your Blog:");
+    newH1.textContent = titleText;
+    // Put that title into an h1 that is centered on the page.
+    newH1.classList.add("text-center");
+    newDiv.appendChild(newH1);
+    body.insertBefore(newDiv, myBtn)
+}
+function generateP(){
+  // Ask the user to write a few sentences about what they did today.
+  let paraText = prompt("Please describe your day:");
+  newP.textContent = paraText;
+  // Populate a p tag with the sentences, align the p tag text with justify.
+  newP.classList.add("text-justify");
+  generateBG()
+  newDiv.appendChild(newP);
+  body.insertBefore(newDiv, myBtn)
+}
+
+function generateBG(){
+  // Finally, ask the user for his or her favorite color,
+  let i = 0;
+  while (i < 1) {
+  let color = prompt(`What is your favorite color? \n IE: red, orange, yellow, green, blue, pink, purple, black, white, or grey`).toLowerCase();
+    switch (color) {
+      case "red":
+        newP.style.backgroundColor = "red"
+        i++
+        break;
+      case "orange":
+        newP.style.backgroundColor = "orange"
+        i++
+        break;
+      case "yellow":
+        newP.style.backgroundColor = "yellow"
+        i++
+        break;
+      case "green":
+        newP.style.backgroundColor = "green"
+        i++
+        break;
+      case "blue":
+        newP.style.backgroundColor = "blue"
+        i++
+        break;
+      case "pink":
+        newP.style.backgroundColor = "pink"
+        i++
+        break;
+      case "purple":
+        newP.style.backgroundColor = "purple"
+        i++
+        break;
+      case "black":
+        newP.style.backgroundColor = "black"
+        newP.style.color = "white"
+        i++
+        break;
+      case "white":
+        newP.style.backgroundColor = "white"
+        i++
+        break;
+      case "grey":
+        newP.style.backgroundColor = "grey"
+        i++
+        break;
+        // Remember, check for edge cases with color.
+      default: let color = alert(`That is not a defined color, try again!`);
+    }
+
+  }
+
+  //and set the background of the p tag to that color
+
+}
+
+
+myBtn.addEventListener("click", function(){
+  generateH1();
+  generateP();
+});
